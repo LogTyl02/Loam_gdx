07f681c980a597529a656a1911cc74c240704446
LogTyl02 <logtyl02@evergreen.edu>
2014-01-30 21:16:55 -0800

    fonts

--- Loam-desktop/src/com/almanac/loam/Main.java
+++ Loam-desktop/src/com/almanac/loam/Main.java
@@ -8,8 +8,8 @@ public class Main {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "Loam " + Loam.VERSION;
 		cfg.useGL20 = true;
-		cfg.width = 800;
-		cfg.height = 600;
+		cfg.width = 1400;
+		cfg.height = 800;
 		
 		new LwjglApplication(new Loam(), cfg);
 	}
